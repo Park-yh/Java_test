58cc6ccbd1e68ce6427135f1ae1ab27ad698eaa0
Park-yh <pyh5421@naver.com>
2025-07-29 11:36:56 +0900

    Q4 완료

---
+++ Java-test/src/Q4/Team.java
@@ -0,0 +1,28 @@
+package Q4;
+
+import java.util.ArrayList;
+
+class Team {
+    String teamName;
+    /* TODO: 올바른 타입의 필드(속성) 선언 변수명은 playerList; */
+    ArrayList<Player> playerList;
+
+    public Team(String teamName) {
+        this.teamName = teamName;
+        /* TODO: 위에서 선언한 playerList 필드 초기화 */
+        this.playerList = new ArrayList<>();
+    }
+
+    public void addPlayer(Player player/* TODO: 올바른 매개변수를 선언하세요 */) {
+        // TODO: 매개변수로 받은 player 객체를 playerList에 추가하세요.
+        this.playerList.add(player);
+    }
+
+    public void printPlayerList() {
+        // TODO: 반복문을 사용하여 playerList에 있는 모든 선수의 이름을 출력하세요.
+        for(Player p : playerList){
+            System.out.println("선수 이름: " + p.name);
+        }
+        }
+    }
+
